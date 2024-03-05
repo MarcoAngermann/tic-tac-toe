51d56a828a9826cb457f80b99f246d0076df55c4
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-03-05 13:30:00 +0100

    svg function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let fields = [
     null,
-    null,
-    null,
+    'circle',
+    'cross',
     null,
     null,
     null,
@@ -23,9 +23,9 @@ function render() {
             let index = i * 3 + j;
             let symbol;
             if (fields[index] === 'circle') {
-                symbol = 'o';
+                symbol = generateCircleSVG();
             } else if (fields[index] === 'cross') {
-                symbol = 'x';
+                symbol = generateCrossSVG();
             } else {
                 symbol = '';
             }
@@ -36,4 +36,35 @@ function render() {
     tableHtml += '</table>';
     content.innerHTML = tableHtml;
 }
+
+function generateCircleSVG() {
+    let width = 70;
+    let height = 70;
+    let color = "#00B0EF";
+
+    const svgCode = `
+        <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
+            <circle cx="${width / 2}" cy="${height / 2}" r="${width / 2 - 5}" fill="none" stroke="#FFFFFF" stroke-width="5">
+                <animate attributeName="r" from="0" to="${width / 2 - 5}" dur="1s" fill="freeze" />
+                <animate attributeName="stroke" from="#FFFFFF" to="${color}" dur="1s" fill="freeze" />
+            </circle>
+        </svg>
+    `;
+    return svgCode;
+}
+
+function generateCrossSVG() {
+    let width = 70;
+    let height = 70;
+    let color = "#FEC146";
+
+    const svgCode = `
+        <svg class="cross" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0" y1="0" x2="${width}" y2="${height}" stroke="${color}" stroke-width="5" />
+            <line x1="${width}" y1="0" x2="0" y2="${height}" stroke="${color}" stroke-width="5" />
+        </svg>
+    `;
+    return svgCode;
+}
+
   
